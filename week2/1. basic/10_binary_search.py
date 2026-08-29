7d65562352d3fc79019fb3cc97108765548d7fe2
dongho <jijiji0809160@gmail.com>
2026-08-29 22:31:43 +0900

    10

--- week2/1. basic/10_binary_search.py
+++ week2/1. basic/10_binary_search.py
@@ -23,28 +23,22 @@
 """
 
 def binary_search(arr, target):
-    """
-    이분 탐색 구현
-    
-    Args:
-        arr: 정렬된 배열
-        target: 찾을 값
-    
-    Returns:
-        target의 인덱스 (없으면 -1)
-    """
     left = 0
     right = len(arr) - 1
-    
-    # TODO: left가 right보다 작거나 같을 때까지 반복
-    ## 중간 인덱스 계산
-    ## arr[mid]와 target 비교
-    ## 같으면 mid 반환
-    ## target이 더 크면 left = mid + 1
-    ## target이 더 작으면 right = mid - 1
-    pass
-    
-    return -1
+
+    result = -1
+
+    while left <= right:
+        mid = (right - left) // 2 + left
+        if arr[mid] == target:
+            result = mid
+            break
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
 
 # 테스트 케이스
 if __name__ == "__main__":
