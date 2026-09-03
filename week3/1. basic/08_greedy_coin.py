7b824217be8c95c97c336ef5faf6511d6e91865e
dongho <jijiji0809160@gmail.com>
2026-09-03 22:53:41 +0900

    solving 8

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -47,6 +47,18 @@ def make_change_greedy(change, coins):
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
     pass
+
+    for coin in coins:
+        while True:
+            if change >= coin:
+                change -= coin
+                total_coins+=1
+                if coin in result:
+                    result[coin] += 1
+                else:
+                    result[coin] = 1
+            else:
+                break
     
     return total_coins, result
 
